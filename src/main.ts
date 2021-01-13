70c8d92010432ac364e49eefb9c1486aa995cd37
Rob DeRosa <7773968+rob-derosa@users.noreply.github.com>
2021-01-12 21:00:21 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,6 @@ async function run(): Promise<void> {
     // const gitHubToken = args[2];
     // const failIfViolations = false
 
-    const regEx = new RegExp('^\\s*(\\s*/[*].*[*]/\\s*)*\\}|^\\s*(\\s*/[*].*[*]/\\s*)*\\)|^\\s*(public|private|protected):\\s*$|^\\s*@(public|private|protected)\\s*$');
     const policyType = core.getInput("policy", { required: true })
     const policyUrl = core.getInput("policy-url", { required: true })
     const gitHubToken = core.getInput("github-token", { required: true })
